68be54d0458ee5ad2c035889af754d549283363c
morpheusxx <morpheus_xx@team-mediaportal.com>
2011-11-29 18:52:30 +0100

    Added BluRayPlayer plugin to incubator

---
+++ MediaPortal/Incubator/BluRayPlayer/BluRayEventBuffer.cs
@@ -0,0 +1,57 @@
+﻿using MediaPortal.Common;
+using MediaPortal.Common.Logging;
+
+namespace MediaPortal.UI.Players.Video
+{
+  public class BluRayEventBuffer
+  {
+    const int SIZE = 128;
+    readonly BluRayAPI.BluRayEvent[] _buffer = new BluRayAPI.BluRayEvent[SIZE];
+    int _readPos = 0;
+    int _writePos = 0;
+
+    public bool IsEmpty()
+    {
+      return _readPos == _writePos;
+    }
+
+    public int Count
+    {
+      get
+      {
+        int len = _writePos - _readPos;
+        if (len < 0)
+          len += SIZE;
+        return len;
+      }
+    }
+
+    public void Clear()
+    {
+      _writePos = 0;
+      _readPos = 0;
+    }
+
+    public void Set(BluRayAPI.BluRayEvent data)
+    {
+      _buffer[_writePos] = data;
+      _writePos = (_writePos + 1) % SIZE;
+      if (_readPos == _writePos)
+      {
+        ServiceRegistration.Get<ILogger>().Warn("BluRayPlayer: Event buffer full");
+      }
+    }
+
+    public BluRayAPI.BluRayEvent Peek()
+    {
+      return _buffer[_readPos];
+    }
+
+    public BluRayAPI.BluRayEvent Get()
+    {
+      int pos = _readPos;
+      _readPos = (_readPos + 1) % SIZE;
+      return _buffer[pos];
+    }
+  }
+}
---
+++ MediaPortal/Incubator/BluRayPlayer/BluRayInterfaces.cs
@@ -0,0 +1,97 @@
+﻿#region Copyright (C) 2007-2011 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2011 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace MediaPortal.UI.Players.Video
+{
+  [ComVisible(true), ComImport, Guid("324FAA1F-4DA6-47B8-832B-3993D8FF4151"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+  public interface IBDReaderCallback
+  {
+    [PreserveSig]
+    int OnMediaTypeChanged(BluRayAPI.VideoRate videoRate, BluRayAPI.BluRayStreamFormats videoFormat, BluRayAPI.BluRayStreamFormats audioFormat);
+
+    [PreserveSig]
+    int OnBDevent([Out] BluRayAPI.BluRayEvent bluRayEvent);
+
+    [PreserveSig]
+    int OnOSDUpdate([Out] BluRayAPI.OSDTexture osdInfo);
+  }
+
+  [Guid("79A37017-3178-4859-8079-ECB9D546FEC2"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+  public interface IBDReader
+  {
+    [PreserveSig]
+    int SetBDReaderCallback(IBDReaderCallback callback);
+
+    [PreserveSig]
+    int Action(int key);
+
+    [PreserveSig]
+    int SetAngle(int angle);
+
+    [PreserveSig]
+    int SetChapter(uint chapter);
+
+    [PreserveSig]
+    int GetAngle(ref int angle);
+
+    [PreserveSig]
+    int GetChapter(ref uint chapter);
+
+    [PreserveSig]
+    int GetTitleCount(ref uint count);
+
+    [PreserveSig]
+    IntPtr GetTitleInfo(int index);
+
+    [PreserveSig]
+    int GetCurrentClipStreamInfo(ref BluRayAPI.BDStreamInfo stream);
+
+    [PreserveSig]
+    int FreeTitleInfo(IntPtr info);
+
+    [PreserveSig]
+    int OnGraphRebuild(int info);
+
+    [PreserveSig]
+    int ForceTitleBasedPlayback(bool force, UInt32 title);
+
+    [PreserveSig]
+    int SetD3DDevice(IntPtr d3dDevice);
+
+    [PreserveSig]
+    int SetBDPlayerSettings(BluRayAPI.BDPlayerSettings settings);
+
+    [PreserveSig]
+    int Start();
+
+    [PreserveSig]
+    int MouseMove(int x, int y);
+  }
+
+  [ComImport, Guid(BluRayAPI.BDREADER_GUID)]
+  public class BDReader { }
+}
